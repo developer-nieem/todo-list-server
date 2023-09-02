fb050add694c4f433c7813dc4f83b278dcb91761
developer-nieem <naimhossenwork@gmail.com>
2023-09-02 23:49:50 +0600

    added Crud oparations

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xifd9dy.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,9 +33,33 @@ app.get("/todo-list" , async(req, res) =>{
     res.send(result);
 })
 
+app.post("/todo-list", async(req, res) =>{
+    const body =  req.body;
+    const result =  await todoListCollectionDB.insertOne(body);
+    res.send(result)
+})
 
+app.put("/todo-list/:id" , async(req, res) => {
+    const id =  req.params.id;
+    const body = req.body
+    const filter = {_id: ObjectId(id)};
+    const updateDoc = {
+        $set: {
+            title: body.title,
+            details: body.details,
+            status: body.status
+        }
+    }
+    const result =  await todoListCollectionDB.updateOne(filter, updateDoc);
+    res.send(result)
+})
 
-
+app.delete('/todo-list/:id' , async(res , req) =>{
+    const id =  req.params.id;
+    const filter =  {_id: ObjectId(id)}
+    const result =  await todoListCollectionDB.deleteOne(filter);
+    res.send(result)
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
